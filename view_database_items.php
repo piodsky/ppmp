5dd7bbd36295b218cf273931910854821256e273
theo <piodskybaby@gmail.com>
2025-11-13 14:15:45 +0800

    new design

--- view_database_items.php
+++ view_database_items.php
@@ -85,7 +85,7 @@ $user_role = $role;
     <!-- Font Awesome -->
     <link rel="stylesheet" href="assets/font-awesome.min.css">
     <!-- DataTables CSS -->
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.13.7/css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" href="assets/dataTables.bootstrap5.min.css">
     <!-- Favicon -->
     <link rel="icon" type="image/svg+xml" href="assets/logo.svg">
 
@@ -719,9 +719,9 @@ $user_role = $role;
 <script src="argondashboard/assets/js/argon-dashboard.min.js"></script>
 
 <!-- DataTables JS -->
-<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-<script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap5.min.js"></script>
+<script src="assets/jquery-3.7.1.min.js"></script>
+<script src="assets/jquery.dataTables.min.js"></script>
+<script src="assets/dataTables.bootstrap5.min.js"></script>
 
 <!-- Token Manager for authentication - only include necessary functions -->
 <script>
